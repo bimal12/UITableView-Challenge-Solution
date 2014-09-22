a644bb825aeb160a17c8fbce044e24989a2d1bb0
Bimal Shah <bimalshah@bimalpc.home>
2014-09-22 14:20:13 +0100

    extracredit

--- UITableViewController Challenge Solution/BSUITableViewController.m
+++ UITableViewController Challenge Solution/BSUITableViewController.m
@@ -78,14 +78,17 @@
     if(indexPath.section == 0)
     {
         cell.textLabel.text = @"I am in section 0";
+        cell.backgroundColor = [UIColor redColor];
     }
     else if (indexPath.section == 1)
     {
         cell.textLabel.text = @"another section";
+        cell.backgroundColor = [UIColor blueColor];
     }
     else if (indexPath.section==2)
     {
         cell.textLabel.text = [NSString stringWithFormat:@"Cell %i", indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
     }
     return cell;
 }
